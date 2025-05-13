f24fc8cf7cf48623207420f0e697780e935208b3
RodrigoLCJunior <rodrigo.junior.rc@gmail.com>
2025-05-13 12:19:16 -0300

    Foi triste mas deu certo

---
+++ demo/demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import com.example.demo.security.JwtUtil;
+import com.example.demo.service.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.AuthenticationException;
+
+@RestController
+@RequestMapping("")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired private CustomUserDetailsService userDetailsService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        String token = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+}
+
+@Data
+class AuthRequest {
+    private String username;
+    private String password;
+}
+
+@Data
+@AllArgsConstructor
+class AuthResponse {
+    private String token;
+}
